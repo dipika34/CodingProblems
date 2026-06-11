7b9b102118506658c1e7c578eed89b84026e2485
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-06-12 00:35:14 +0530

    Create OddSumArrayElement.c

---
+++ OddSumArrayElement.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("How many elements?");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int sum = 0, temp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            sum += arr[i];
+        
+        }
+     
+    }
+    printf("Sum of odd elements in the array: %d", sum);
+    return 0;
+}
