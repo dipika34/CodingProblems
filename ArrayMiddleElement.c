6e5cee5d4ec77a05243ff07f26a1025ae62f9524
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-06-12 09:46:17 +0530

    Create ArrayMiddleElement.c

---
+++ ArrayMiddleElement.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    printf("How many elements?");
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    int start = 0;
+    int end = n - 1;
+    int middle = 0;
+    int temp = 0;
+    printf("\nEnter the elements in the array : ");
+    for (int i = start; i <= end; i++)
+
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\nThe elements in the array :\n");
+    for (int i = start; i <= end; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    
+    temp = (start + end) / 2;
+    middle = arr[temp];
+
+    printf("The middle element in the array is : index = %d element =  %d", temp, middle);
+    return 0;
+}
